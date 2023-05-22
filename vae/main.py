2cbe36c41e14755ad0da95aaae3916d6687d6595
akos-ruzsa-technolynx <akos.ruzsa@technolynx.com>
2023-05-22 17:56:30 +0200

    update model

--- vae/main.py
+++ vae/main.py
@@ -10,12 +10,17 @@ from tensorflow_addons.layers import SpectralNormalization, InstanceNormalizatio
 import os
 from tqdm import tqdm
 
-DATA_FOLDER = "datasets/celeba_256_1000"
+DATA_FOLDER = "datasets/celeba_hq_256"
 IMAGE_SIZE = 256
 LATENT_DIM = 256
 LEARNING_RATE = 0.00001
-BATCH_SIZE = 1
-EPOCHS = 10
+BATCH_SIZE = 8
+EPOCHS = 300
+
+
+VGG_LOSS_MULTIPLIER = 1
+RECON_LOSS_MULTIPLIER = 0.5
+KLD_LOSS_MULTIPLIER = 0.0001
 
 
 def encoder_model():
@@ -27,32 +32,31 @@ def encoder_model():
     x = SpectralNormalization(Conv2D(128, kernel_size=3, strides=2, activation='leaky_relu', kernel_initializer='he_normal', padding='same'))(x)
     x = InstanceNormalization()(x)
     x = SpectralNormalization(Conv2D(128, kernel_size=3, strides=2, activation='leaky_relu', kernel_initializer='he_normal', padding='same'))(x)
-    x = InstanceNormalization()(x)
     x = SpectralNormalization(Conv2D(128, kernel_size=3, strides=2, activation='leaky_relu', kernel_initializer='he_normal', padding='same'))(x)
-    x = InstanceNormalization()(x)
     x = SpectralNormalization(Conv2D(128, kernel_size=3, strides=2, activation='leaky_relu', kernel_initializer='he_normal', padding='same'))(x)
-    x = InstanceNormalization()(x)
     x = Flatten()(x)
-    mean = Dense(LATENT_DIM, kernel_initializer='he_normal')(x)
-    logvar = Dense(LATENT_DIM, kernel_initializer='he_normal')(x)
+    mean = SpectralNormalization(Dense(LATENT_DIM, kernel_initializer='he_normal'))(x)
+    logvar = SpectralNormalization(Dense(LATENT_DIM, kernel_initializer='he_normal'))(x)
     return Model(inputs=image, outputs=[mean, logvar])
 
 
-def decoder_block(res, img, channels, lv):
-    res = UpSampling2D(2, interpolation='bicubic')(res)
+def decoder_block(res, channels, lv, norm):
+    interpolation = 'nearest' if res.shape[1] < IMAGE_SIZE // 4 else 'bicubic'
+    res = UpSampling2D(2, interpolation=interpolation)(res)
     res = SpectralNormalization(Conv2D(channels, kernel_size=3, activation='leaky_relu', kernel_initializer='he_normal', padding='same'))(res)
-    res = InstanceNormalization(center=False, scale=False)(res)
 
-    common_dense = Dense(512, activation='leaky_relu', kernel_initializer='he_normal')(lv)
-    beta = Dense(channels, kernel_initializer='he_normal')(common_dense)
-    beta = Reshape((1, 1, channels))(beta)
-    gamma = Dense(channels, kernel_initializer='he_normal')(common_dense)
-    gamma = Reshape((1, 1, channels))(gamma)
-    res = res * (1 + gamma) + beta
+    if norm:
+        res = InstanceNormalization(center=False, scale=False)(res)
 
-    out_res = Conv2D(3, kernel_size=1, kernel_initializer='he_normal')(res)
-    img = UpSampling2D(2, interpolation='bicubic')(img)
-    img = img + out_res
+        common_dense = SpectralNormalization(Dense(channels * 4, activation='leaky_relu', kernel_initializer='he_normal'))(lv)
+        common_dense = InstanceNormalization()(common_dense)
+        beta = SpectralNormalization(Dense(channels, kernel_initializer='he_normal'))(common_dense)
+        beta = Reshape((1, 1, channels))(beta)
+        gamma = SpectralNormalization(Dense(channels, kernel_initializer='he_normal'))(common_dense)
+        gamma = Reshape((1, 1, channels))(gamma)
+        res = res * (1 + gamma) + beta
+
+    img = Conv2D(3, kernel_size=1, activation='tanh', kernel_initializer='glorot_normal')(res)
     return res, img
 
 
@@ -63,26 +67,26 @@ def decoder_model():
     res = SpectralNormalization(Dense(4 * 4 * 128, activation='leaky_relu', kernel_initializer='he_normal'))(lv)
     res = Reshape((4, 4, 128))(res)
     res = SpectralNormalization(Conv2D(128, kernel_size=3, activation='leaky_relu', kernel_initializer='he_normal', padding='same'))(res)
-    res = InstanceNormalization(center=False, scale=False)(res)
+
     img = Conv2D(3, kernel_size=1, kernel_initializer='he_normal')(res)
     outputs.append(img)
 
-    res, img = decoder_block(res, img, 128, lv)
+    res, img = decoder_block(res, 128, lv, False)
     outputs.append(img)
 
-    res, img = decoder_block(res, img, 128, lv)
+    res, img = decoder_block(res, 128, lv, False)
     outputs.append(img)
 
-    res, img = decoder_block(res, img, 128, lv)
+    res, img = decoder_block(res, 128, lv, True)
     outputs.append(img)
 
-    res, img = decoder_block(res, img, 128, lv)
+    res, img = decoder_block(res, 128, lv, True)
     outputs.append(img)
 
-    res, img = decoder_block(res, img, 64, lv)
+    res, img = decoder_block(res, 128, lv, True)
     outputs.append(img)
 
-    res, img = decoder_block(res, img, 32, lv)
+    res, img = decoder_block(res, 128, lv, True)
     outputs.append(img)
 
     return Model(inputs=lv, outputs=outputs)
@@ -100,7 +104,7 @@ def batch_array(array):
 def load_batch(file_batch):
     out = []
     for file in file_batch:
-        out.append(cv2.imread(os.path.join(DATA_FOLDER, file)))
+        out.append(cv2.resize(cv2.imread(os.path.join(DATA_FOLDER, file)), (IMAGE_SIZE, IMAGE_SIZE)))
     return np.array(out)
 
 
@@ -111,8 +115,7 @@ def calc_metric(dataset_files):
     dataset_files_batched = batch_array(dataset_files)
     for files in dataset_files_batched:
         images = load_batch(files)
-        images = images.astype(np.float32) / 255
-        images = np.clip(images, -1, 1)
+        images = images.astype(np.float32) / 128 - 1
         mean, _ = encoder(images)
         metric.update_state(images, decoder(mean)[-1])
     return metric.result().numpy()
@@ -131,26 +134,24 @@ def loss_fn(x, recons, mean, logvar):
     for level in recons[::-1]:
         recon_loss += l1_loss(level, x_copy)
         x_copy = tf.image.resize(x_copy, (x_copy.shape[1] // 2, x_copy.shape[2] // 2), method='bicubic')
-
     recon_loss /= len(recons)
 
     kld_loss = -0.5 * tf.reduce_sum(1 + logvar - tf.pow(mean, 2) - tf.exp(logvar))
     kld_loss /= BATCH_SIZE
 
+    vgg_loss = 0
     vgg_x = vgg19(x)
     vgg_recon = vgg19(recons[-1])
-    vgg_loss = 0
     for i in range(len(vgg_x)):
         vgg_loss += l1_loss(vgg_x[i], vgg_recon[i])
     vgg_loss /= len(vgg_x)
 
-    loss = recon_loss + kld_loss + vgg_loss
-    print(loss.numpy())
+    loss = RECON_LOSS_MULTIPLIER * recon_loss + VGG_LOSS_MULTIPLIER * vgg_loss + KLD_LOSS_MULTIPLIER * kld_loss
     return loss
 
 
 def train_for_one_batch(batch):
-    batch = batch.astype(np.float32) / 255
+    batch = batch.astype(np.float32) / 128 - 1
 
     with tf.GradientTape() as tape_encoder, tf.GradientTape() as tape_decoder:
         mean, logvar = encoder(batch)
@@ -183,8 +184,11 @@ def train():
         encoder.save("encoder.hd5")
         decoder.save("decoder.hd5")
         print(f"MSE = {mse[-1]}")
+
+    # encoder.save("encoder.hd5")
+    # decoder.save("decoder.hd5")
     plt.plot(mse)
-    plt.show()
+    plt.savefig('loss.png')
 
 
 def test():
@@ -195,12 +199,12 @@ def test():
     data_files = np.array(os.listdir(DATA_FOLDER))
     np.random.shuffle(data_files)
     for file in tqdm(data_files):
-        image = cv2.imread(os.path.join(DATA_FOLDER, file))
+        image = cv2.resize(cv2.imread(os.path.join(DATA_FOLDER, file)), (IMAGE_SIZE, IMAGE_SIZE))
         cv2.imshow("original", image)
-        image = image.astype(np.float32) / 255
+        image = image.astype(np.float32) / 128 - 1
         mean, _ = encoder(image.reshape(1, IMAGE_SIZE, IMAGE_SIZE, 3))
         recon = decoder(mean)[-1].numpy()
-        recon = (recon.reshape(IMAGE_SIZE, IMAGE_SIZE, 3) * 255).clip(0, 255).astype(np.uint8)
+        recon = ((recon.reshape(IMAGE_SIZE, IMAGE_SIZE, 3) + 1) * 128).clip(0, 255).astype(np.uint8)
         cv2.imshow("reconstruction", recon)
         cv2.waitKey(0)
 
@@ -211,7 +215,7 @@ def test_random_latent():
     while True:
         latent = np.random.random((1, LATENT_DIM))
         image = decoder(latent)[-1].numpy()
-        image = (image.reshape(IMAGE_SIZE, IMAGE_SIZE, 3) * 255).clip(0, 255).astype(np.uint8)
+        image = ((image.reshape(IMAGE_SIZE, IMAGE_SIZE, 3) + 1) * 128).clip(0, 255).astype(np.uint8)
         cv2.imshow("generated image", image)
         cv2.waitKey(0)
 
@@ -226,12 +230,12 @@ decoder.summary()
 # decoder = keras.models.load_model("decoder.hd5")
 # decoder.summary()
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=LEARNING_RATE)
 vgg19 = VGG19(include_top=False, weights='imagenet', input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
 vgg19_selectedLayers = [1, 2, 9, 10, 17, 18]
 vgg19_selectedOutputs = [vgg19.layers[i].output for i in vgg19_selectedLayers]
 vgg19 = Model(vgg19.inputs, vgg19_selectedOutputs)
 
 train()
-test()
+# test()
 # test_random_latent()
