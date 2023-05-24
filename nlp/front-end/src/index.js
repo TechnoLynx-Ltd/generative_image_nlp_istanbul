3c5f0ad1c2dc926bf1bdd2f6939aae585487f169
Hanadi Solieman <122284349+hanadi-solieman-technolynx@users.noreply.github.com>
2023-05-24 13:13:04 +0200

    adding new features
    Added more facial features with different components based on the category type. fixed the collapsing of the different accordion elements

--- nlp/front-end/src/index.js
+++ nlp/front-end/src/index.js
@@ -1,6 +1,5 @@
 function pictureChange() {
     document.getElementById("generated_image").src = '../michael-1.jpg';
-    document.write(document.getElementById("race").value);
 }
 function tabular_data_creation() {
     var race = document.getElementById("race").value;
