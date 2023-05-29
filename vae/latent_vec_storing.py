20f8a892ae1e94fa49e11b66a68dc331a86c3db9
Hanadi Solieman <122284349+hanadi-solieman-technolynx@users.noreply.github.com>
2023-05-29 12:54:33 +0200

    updating latent script with avg calc
    updating the latent_vec_storing.py to store the avg latent vector of features

--- vae/latent_vec_storing.py
+++ vae/latent_vec_storing.py
@@ -13,58 +13,25 @@ def latent_interpolate(vec1, vec2, inter_steps=5):
         inter_vectors.append(vec)
     return np.asarray(inter_vectors)
 
-def latent_clustering(attribute_num):
-    latent_clusters = []
-    young_count = 0
-    middleAge_count = 0
-    senior_count = 0
-    young_latent = np.zeros((1,256))
-    middleAge_latent = np.zeros((1,256))
-    senior_latent = np.zeros((1,256))
-
-    latent_vec_path = "vae\latent.csv"
-    data_latent = np.genfromtxt(latent_vec_path, dtype=None, delimiter=',', names=True, encoding='UTF-8')
-
-    # getting the  vectors of age (young, middle-aged, and senior)
-    for i in range(data_latent.shape[0]):
-        if (data_latent[i][2] == 1):
-            young_latent += data_latent[i][48]
-            young_count += 1
-
-        elif (data_latent[i][3] == 1):
-            middleAge_latent += data_latent[i][48]
-            middleAge_count += 1
-
-        elif (data_latent[i][4] == 1):
-            senior_latent += data_latent[i][48]
-            senior_count += 1
-
-    young_latent /= young_count
-    latent_clusters.append(young_latent)
-    middleAge_latent /= middleAge_count
-    latent_clusters.append(middleAge_latent)
-    senior_latent /= senior_count
-    latent_clusters.append(senior_latent)
-
-    return 
-    # getting the vectors of biological sex (male/ female)
-    for i in range(data_latent.shape[0]):
-        if (data_latent[i][1])
-    # getting the vectors of eyewear (no eyewear, eyeglasses)
-    # getting the vector of facial hair (no beard, mustache, goatee, beard)
-    # getting the vector of face shape (oval, round, square)
-    # getting the vector of lipstick (yes, no)
-    return
 
 IMAGE_SIZE = 256
 cols_names =[]
 out_row = []
+# male_count = 0
+# male_latent = np.zeros((1,256))
+young_count = 0
+young_latent = np.zeros((1,256))
+middleAge_count = 0
+middleAge_latent = np.zeros((1,256))
+senior_count = 0
+senior_latent = np.zeros((1,256))
+
 dataset_folder = "vae\data"
 Metadata_file_path ="vae\Metadata.csv"
 
 encoder = keras.models.load_model("vae\encoder.hd5")
 
-Metadata = np.genfromtxt(Metadata_file_path, dtype=None, delimiter=',', names=True, encoding='UTF-8', max_rows=10)
+Metadata = np.genfromtxt(Metadata_file_path, dtype=None, delimiter=',', names=True, encoding='UTF-8')
 for j in range(49):
     if j != 1:
         cols_names.append(Metadata.dtype.names[j])
@@ -81,9 +48,31 @@ with open("vae\latent.csv", "w", newline="") as f:
         mean, _ = encoder(img.reshape(1, IMAGE_SIZE, IMAGE_SIZE, 3))
         out_row.append(np.asarray(mean))
         writer.writerow(out_row)
+        # calculating the avg latent for features
+        # if (Metadata[i][2] == 1):
+        #     male_latent += np.asarray(mean)
+        #     male_count += 1
+        # if (Metadata[i][2] == -1):
+        #     female_latent += np.asarray(mean)
+        #     female_count += 1
+        if (Metadata[i][3] == 1):
+            young_latent += np.asarray(mean)
+            young_count += 1
+        if (Metadata[i][4] == 1):
+            middleAge_latent += np.asarray(mean)
+            middleAge_count += 1
+        if (Metadata[i][5] == 1):
+            senior_latent += np.asarray(mean)
+            senior_count += 1
 f.close()
-
-
+young_latent /= young_count
+middleAge_latent /= middleAge_count
+senior_latent /= senior_count
+with open("vae\latent_avg.csv", "w", newline="") as l:
+    writer_l = csv.writer(l)
+    writer_l.writerow(young_latent)
+    writer_l.writerow(middleAge_latent)
+    writer_l.writerow(senior_latent)
         
 
             
